e7ace6fa3631cd4a86016127251bd6190fa46ce7
Peyman Hosseini <peyman.66h@gmail.com>
2018-12-05 09:10:46 +0200

    login form error handeling

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -10,14 +10,31 @@ if(isset($_POST["submit"]))
         $pwd = mysqli_real_escape_string($con, $_POST['password']);
         
         //error handeling
-        if (empty($email) || empty($pwd)){
-            header("Location: ../signup.php?signup=empty");
-                exit();
-        }else{
+        if (empty($email) || empty($pwd))
+        {
+            header("Location: ../login.php?login=empty");
+            exit();
+        }
+        //Password must contain 6 characters of letters, numbers and at least one special character
+        elseif(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%])[0-9A-Za-z!@#$%]{6,15}$/', $pwd))
+        {
+            header("Location: ../login.php?login=character");
+            exit();
+        }
+        //check email format 
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            
+            header("Location: ../login.php?login=invalidEmail");
+            exit();
+        }
+        
+        
+           else{ 
             $sql = "SELECT * FROM student_table WHERE std_email = '$email' ";
             $result = mysqli_query ($con , $sql);
             $resultcheck = mysqli_num_rows($result);
-            if($resultcheck < 0){
+            if($resultcheck == 0){
                 header("Location: ../login.php?login=EmailNotFound");
                 exit();
             }else {
@@ -30,22 +47,19 @@ if(isset($_POST["submit"]))
                      exit();
                     } elseif($hashchek == true){
                     // log in user here
-                    /*$_SESSION['student_id'] = $row['std_id'];
+                    $_SESSION['student_id'] = $row['std_id'];
                     $_SESSION['student_name'] = $row['std_name'];
                     $_SESSION['student_fname'] = $row['std_fname'];
                     $_SESSION['student_email'] = $row['std_email'];
-                    $_SESSION['student_cource'] = $row['std_course'];*/
+                    $_SESSION['student_cource'] = $row['std_course'];
                     header("Location: ../student.php?login=success");
                     exit();
                 }
             }
-        }
-    }
+        }}
 }
-        else {
-            header("Location: ../login.php?login=error");
-            exit();
-            }
+
+        
 
 
 
