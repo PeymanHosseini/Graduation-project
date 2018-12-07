4ce4b4838eec47b7adf15b486f7ba3b85d08eae1
Peyman Hosseini <peyman.66h@gmail.com>
2018-12-07 10:16:02 +0200

    session for each user level

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,60 +3,125 @@ session_start();
 
 if(isset($_POST["submit"]))
 {
-//connect to database
-include 'db.inc.php';
-$email = mysqli_real_escape_string($con, $_POST['email']);
-$pwd = mysqli_real_escape_string($con, $_POST['password']);
-        
-        //error handeling
-        if (empty($email) || empty($pwd))
+    //connect to database
+    include 'db.inc.php';
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $pwd = mysqli_real_escape_string($con, $_POST['password']);
+    
+    //error handeling
+    if (empty($email) || empty($pwd))
+    {
+        header("Location: ../login.php?login=emptyyyy");
+        exit();  
+    }
+
+    //Password must contain 6 characters of letters, numbers and at least one special character
+    //elseif(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%])[0-9A-Za-z!@#$%]{6,15}$/', $pwd))
+    //{
+     //   header("Location: ../login.php?login=character");
+      //  exit();
+   // }
+
+    //check email format 
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        header("Location: ../login.php?login=invalidEmail");
+        exit();
+    }
+
+    else
+    {
+        //admin
+        $query1 = mysqli_query($con, "SELECT * FROM staff_table WHERE staff_email='$email' AND password='$pwd'");
+        if(mysqli_num_rows($query1) == 0)
         {
-            header("Location: ../login.php?login=emptyyyy");
-            exit();
+            header("Location: ../login.php?login=adminUserNotValid");
             
         }
-        //Password must contain 6 characters of letters, numbers and at least one special character
-        elseif(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%])[0-9A-Za-z!@#$%]{6,15}$/', $pwd))
+        else
         {
-            header("Location: ../login.php?login=character");
-            exit();
-        }
-        //check email format 
-        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
-        {
+            $row1 = mysqli_fetch_assoc($query1);
+            $_SESSION['email']=$row1['staff_email'];
+            $_SESSION['admin'] = $row1['level'];
             
-            header("Location: ../login.php?login=invalidEmail");
-            exit();
-        }
-
-           else{ 
-            $sql = "SELECT * FROM student_table WHERE std_email = '$email' ";
-            $result = mysqli_query ($con , $sql);
-            $resultcheck = mysqli_num_rows($result);
-            if($resultcheck == 0){
-                header("Location: ../login.php?login=EmailNotFound");
-                exit();
-            }else {
-                if($row = mysqli_fetch_assoc($result)){
-                //de-hash pass
-                $hashchek = password_verify($pwd, $row['password']);
-               
-                    if($hashchek == false){
-                     header("Location: ../login.php?login=pwdNotM");
-                     exit();
-                    } elseif($hashchek == true){
-
-                    // log in user here
-                    $_SESSION['student_id'] = $row['std_id'];
-                    $_SESSION['student_name'] = $row['std_name'];
-                    $_SESSION['student_fname'] = $row['std_fname'];
-                    $_SESSION['student_email'] = $row['std_email'];
-                    $_SESSION['student_cource'] = $row['std_course'];
-                    header("Location: ../student.php?login=success");
-                    exit();
-                }
+            if($_SESSION['admin'] == $row1['level'])
+            {
+                header("Location: ../admin.php");
             }
-        }}
+            else
+            {
+                $error = "Failed Login";
+            }
+        }
+
+        //advisor
+        $query1 = mysqli_query($con, "SELECT * FROM staff_table WHERE staff_email='$email' AND password='$pwd'");
+        if(mysqli_num_rows($query1) == 0)
+        {
+            header("Location: ../login.php?login=advisorUserNotValid");
+            
+        }
+        else
+        {
+            $row1 = mysqli_fetch_assoc($query1);
+            $_SESSION['email']=$row1['staff_email'];
+            $_SESSION['advisor'] = $row1['level'];
+            
+            if($_SESSION['advisor'] == $row1['level'])
+            {
+                header("Location: ../advisor.php");
+            }
+            else
+            {
+                $error = "Failed Login";
+            }
+        }
+        //committe
+        $query1 = mysqli_query($con, "SELECT * FROM staff_table WHERE staff_email='$email' AND password='$pwd'");
+        if(mysqli_num_rows($query1) == 0)
+        {
+            header("Location: ../login.php?login=committeeUserNotValid");
+            
+        }
+        else
+        {
+            $row1 = mysqli_fetch_assoc($query1);
+            $_SESSION['email']=$row1['staff_email'];
+            $_SESSION['committee'] = $row1['level'];
+            
+            if($_SESSION['committee'] == $row1['level'])
+            {
+                header("Location: ../commitee.php");
+            }
+            else
+            {
+                $error = "Failed Login";
+            }
+        }
+
+         //student
+         $query1 = mysqli_query($con, "SELECT * FROM staff_table WHERE staff_email='$email' AND password='$pwd'");
+         if(mysqli_num_rows($query1) == 0)
+         {
+             header("Location: ../login.php?login=committeeUserNotValid");
+             
+         }
+         else
+         {
+             $row1 = mysqli_fetch_assoc($query1);
+             $_SESSION['email']=$row1['std_email'];
+             $_SESSION['student'] = $row1['level'];
+             
+             if($_SESSION['student'] == $row1['level'])
+             {
+                 header("Location: ../student.php");
+             }
+             else
+             {
+                 $error = "Failed Login";
+             }
+         }
+
+    }
 }
 
-?>
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,18 +1,18 @@
 <?php
 
+if(isset($_POST["signup"]))
+{
 
-if(isset($_POST["signup"])){
-
-//connect to database
+    //connect to database
     include_once 'db.inc.php'; 
-
-// collect the data from form
-$email = mysqli_real_escape_string($con, $_POST['email']);
-$name = mysqli_real_escape_string($con, $_POST['name']);
-$fname = mysqli_real_escape_string($con, $_POST['fname']);
-$tell = mysqli_real_escape_string($con, $_POST['tell']);
-$pwd = mysqli_real_escape_string($con, $_POST['password']);
-$course =$_POST['coursecode'];
+    // collect the data from form
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $fname = mysqli_real_escape_string($con, $_POST['fname']);
+    $tell = mysqli_real_escape_string($con, $_POST['tell']);
+    $pwd = mysqli_real_escape_string($con, $_POST['password']);
+    $course =$_POST['coursecode'];
+    $level= 'student';
 
     // Error handeling
     // check for correct input character
@@ -22,37 +22,35 @@ $course =$_POST['coursecode'];
             exit();
         }
 
-// check if email is valid
-   if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-        {
-            
+    // check if email is valid
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        { 
             header("Location: ../signup.php?signup=invalidEmail");
             exit();
-            
         }
-        else {
-// check if email is existing in database or not
-    $sql = "SELECT * FROM student_table WHERE std_email = '$email' ";
-    $result = mysqli_query ($con , $sql);
-    $resultcheck = mysqli_num_rows($result);
-    if($resultcheck > 0){header("Location: ../signup.php?signup=emailExist");exit();}
-    
+    else
+    {
+        // check if email is existing in database or not
+        $sql = "SELECT * FROM student_table WHERE std_email = '$email' ";
+        $result = mysqli_query ($con , $sql);
+        $resultcheck = mysqli_num_rows($result);
+        if($resultcheck > 0){header("Location: ../signup.php?signup=emailExist");exit();}
 
-        
-// insert data to data base
-        
-            else {
+        // insert data to data base       
+        else
+        {
             //hashing the pass
             $hashedpass = password_hash($pwd,PASSWORD_DEFAULT);
-            $sql = "INSERT INTO `student_table`(`std_email`, `std_name`, `std_fname`, `std_tell`, `password`, `std_course`) VALUES ('$email','$name','$fname','$tell','$hashedpass','$course')";
+            $sql = "INSERT INTO `student_table`(`std_email`,`password`, `std_name`, `std_fname`, `std_tell`, `std_course`,`level`) VALUES ('$email','$hashedpass','$name','$fname','$tell','$course','$level')";
             mysqli_query($con,$sql);
             header("Location: ../login.php");
-            // send email
+            /* send email
             $to = "peyman.h88@hotmail.com";
             $subject = "Graduation Proj";
             $headers = "From: noreply@example.com\r\n";
             $message = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.";        
             mail($to, $subject, $message, $headers);
-
-            }
-        }}
+            */
+        }
+    }
+}
