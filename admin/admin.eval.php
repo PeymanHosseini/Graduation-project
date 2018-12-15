d7f2552eb3822444414d2f5c03e18335b55448ae
Peyman Hosseini <peyman.66h@gmail.com>
2018-12-15 18:21:18 +0200

    replace evaluation form

--- admin/admin.eval.php
+++ admin/admin.eval.php
@@ -5,18 +5,71 @@ include 'header.php';
   <div class="main">
     <div class="mainContent clearfix">
       <div id="dashboard">
-        <h1 class="header"><span class="">helooooo</span></h1>
+        <h1 class="header"><span class=""></span></h1>
         <div class="quick-press">
             <h4>Add/Delete Staff</h4>
             <div class="clearfix">
             <form action="../includes/admin.add.php" method="post">
-             <input type="text" name="email" placeholder="Email"/>
-             <input type="text" name="password" placeholder="Password"/>
-             <input type="text" name="name" placeholder="Name"/>
-             <input type="text" name="surname" placeholder="Surname"/>
-             <input type="text" name="office" placeholder="Office"/>
-             <input type="text" name="level" placeholder="Level"/>
-             <button type="submit" class="submit2" name="submit2">Delete</button>
+            <table style="width:100%">
+              <tr>
+                <th>ID</th>
+                <th>Question</th>
+                <th >Rate</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>DB Evaluation by Committee Members</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Peer Evaluation</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Project Demonstration</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Student Evaluation By Jury Members</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>Documentation</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>Source Code Evaluation By Committee Members</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>Student Evaluation By Supervisor</td>
+                <td><input type="radio" name="gender" value="male"> Good
+             <input type="radio" name="gender" value="female"> Average
+             <input type="radio" name="gender" value="female"> Bad</td>
+              </tr>
+            </table>
+           
+             
+             
+             
              <button type="submit" class="submit" name="submit">Add</button>
            </form>
            </div>
