a9bf904476955d3e2969a543911a8b6640c10932
Peyman Hosseini <peyman.66h@gmail.com>
2018-12-15 18:23:38 +0200

    changing form

--- admin/admin.eval.php
+++ admin/admin.eval.php
@@ -19,51 +19,51 @@ include 'header.php';
               <tr>
                 <td>1</td>
                 <td>DB Evaluation by Committee Members</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="1" value="male"> Good
+             <input type="radio" name="1" value="female"> Average
+             <input type="radio" name="1" value="female"> Bad</td>
               </tr>
               <tr>
                 <td>2</td>
                 <td>Peer Evaluation</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="2" value="male"> Good
+             <input type="radio" name="2" value="female"> Average
+             <input type="radio" name="2" value="female"> Bad</td>
               </tr>
               <tr>
                 <td>3</td>
                 <td>Project Demonstration</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="3" value="male"> Good
+             <input type="radio" name="3" value="female"> Average
+             <input type="radio" name="3" value="female"> Bad</td>
               </tr>
               <tr>
                 <td>4</td>
                 <td>Student Evaluation By Jury Members</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="4" value="male"> Good
+             <input type="radio" name="4" value="female"> Average
+             <input type="radio" name="4" value="female"> Bad</td>
               </tr>
               <tr>
                 <td>5</td>
                 <td>Documentation</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="5" value="male"> Good
+             <input type="radio" name="5" value="female"> Average
+             <input type="radio" name="5" value="female"> Bad</td>
               </tr>
               <tr>
                 <td>6</td>
                 <td>Source Code Evaluation By Committee Members</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="6" value="male"> Good
+             <input type="radio" name="6" value="female"> Average
+             <input type="radio" name="6" value="female"> Bad</td>
               </tr>
               <tr>
                 <td>7</td>
                 <td>Student Evaluation By Supervisor</td>
-                <td><input type="radio" name="gender" value="male"> Good
-             <input type="radio" name="gender" value="female"> Average
-             <input type="radio" name="gender" value="female"> Bad</td>
+                <td><input type="radio" name="7" value="male"> Good
+             <input type="radio" name="7" value="female"> Average
+             <input type="radio" name="7" value="female"> Bad</td>
               </tr>
             </table>
            
