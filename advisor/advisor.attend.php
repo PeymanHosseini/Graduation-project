76717a5ca714daeee4879df0b9647339dbb61e2e
Peyman Hosseini <peyman.66h@gmail.com>
2018-12-22 16:51:51 +0200

    attendance page

--- advisor/advisor.attend.php
+++ advisor/advisor.attend.php
@@ -1,29 +1,52 @@
 <?php
-include("header.advisor.php");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stdsystem";
+$con=mysqli_connect($servername,$username,$password,$dbname);
 
+include("header.advisor.php");
+if(isset($_POST['submit']))
+{
+  $user_id=$_SESSION['email'];
+  $email = $_POST['email'];
+  $course_code=$_POST['coursecode'];
+  $Academic_year=$_POST['academicyear'];
+  // Default to null
+  $date=null;
+  // Only proceed if strtotime() succeeds
+  if($pdate=strtotime($_POST['date'])) {
+  // Since we have a valid time, turn to date string
+  $date=date("Y-m-d", $pdate);
+  }
+    header("location: advisor.attend.php#dashboard.php");
+	
+		mysqli_query($con,"INSERT INTO `attendance`(`std_email`,`staff_email`, `course_id`, `date`) VALUES ('$email','$user_id','$course_code' ,'$date')");
+}
 ?>
 <div class="main">
     
     <div class="mainContent clearfix">
       <div id="dashboard">
-        <h1 class="header"><span class=""></span></h1>
-          <div class="monitor">
-            <h4>Right Now</h4>
-            <div class="clearfix">
-              
-           </div>
-           <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
-         </div>
          <div class="quick-press">
-           <h4>Quick Press</h4>
-           <form action="" method="post">
-             <input type="text" name="title" placeholder="Title"/>
-             <input type="text" name="content" placeholder="Content"/>
-             <input type="text" name="tags" placeholder="Tags"/>
-             <button type="button" class="save">l</button>
-             <button type="button" class="delet">m</button>
-             <button type="submit" class="submit" name="submit">Publish</button>
+           <form action="#" method="post"><br/>
+ <input type="text" name="email" id="username"  placeholder="Enter student Email">  <br/>
+                       <label >Course Code: </label>
+                       <select name="coursecode" >
+                           <option value="1">Itec 403</option>
+                           <option value="2">Itec 404</option>
+                       </select> &nbsp;&nbsp;
+                       <label for="date">   Date:     </label>
+                      <input type="date"  name="date" id="date"/> 
+                      <button type="submit" class="submit" name="submit">Add</button>
            </form>
          </div>
+         </div>
+      </div>
+     </div> 
+   </div>
+</div>
+</body>
+</html>
       
    
