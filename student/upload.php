89e8c2551e62d1890812d701094ec21ad98f36e1
Peyman Hosseini <peyman.66h@gmail.com>
2018-12-23 12:58:29 +0200

    student upload page is done

--- student/upload.php
+++ student/upload.php
@@ -1,21 +1,59 @@
 <?php
-
 include 'header.php';
+include '../includes/db.inc.php';
+// If upload button is clicked ...
+if (isset($_POST['submit'])) 
+{
+$user_id=$_SESSION['email'];
+// Only proceed if strtotime() succeeds
+if($pdate=strtotime($_POST['date'])) {
+  // Since we have a valid time, turn to date string
+  $date=date("Y-m-d", $pdate);}
+  // Get image name
+  $uploadedfile = $_FILES['file']['name'];
+   // Get student id
+   $student_id = mysqli_real_escape_string($con, $_POST['stdNumber']);
+  // Get text
+  $file_text = mysqli_real_escape_string($con, $_POST['file_text']);
+    // image file directory
+  	$target = "uploadedFile/".basename($uploadedfile);
+  $sql = "INSERT INTO uploaded_file (`std_email`, `std_id`, `files`, `date`, `text`) VALUES ('$user_id', '$student_id', '$uploadedfile', '$date', '$file_text')";
+  // execute query
+  mysqli_query($con, $sql);
+  if (move_uploaded_file($_FILES['file']['tmp_name'], $target)) {
+    $msg = "file uploaded successfully";
+  }else{
+    $msg = "Failed to upload file";
+  }
+  
+
+}
+
 ?>
   <div class="main">
     <div class="mainContent clearfix">
       <div id="dashboard">
         <h1 class="header"><span class=""></span></h1>
          <div class="quick-press">
-           <h4>Quick Press</h4>
-           <form action="" method="post">
-             <input type="text" name="title" placeholder="Title"/>
-             <input type="text" name="content" placeholder="Content"/>
-             <input type="text" name="tags" placeholder="Tags"/>
-             <button type="button" class="save">l</button>
-             <button type="button" class="delet">m</button>
-             <button type="submit" class="submit" name="submit">Publish</button>
-           </form>
+           <form action="" method="post" enctype="multipart/form-data"><br/><br/>
+           <label for="date">   Student Number:     </label>
+                 <input type="text" name="stdNumber" ><br/>
+                       <label for="date">   Date:     </label>
+                      <input type="date"  name="date" id="date"/>
+                      <br/><br/>
+                <div>
+                  <input type="file" name="file">
+                </div><br/>
+                <div>
+                  <textarea 
+                    id="text" 
+                    cols="40" 
+                    rows="4" 
+                    name="file_text" 
+                    placeholder="Add extra information"></textarea>
+                </div>
+                <button type="submit" class="submit" name="submit">Upload</button>
+            </form>
          </div>
        </div>
        </div>
