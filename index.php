c6c0386fa3d7adefd522282d74e0e0face152069
Peyman Hosseini <peyman.66h@gmail.com>
2019-01-02 17:22:09 +0200

    index page created

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student Panel</title>
+    <link href="css/Admin.css" type ="text/css" rel="stylesheet">
+</head>
+<body>
+<div class="admin-panel clearfix">
+  <div class="slidebar">
+    <div class="logo">
+      <a href="student403.php#dashboard"></a>
+    </div>
+    <ul >
+        <li><a href="login.php"> Login </a></li>
+        <li><a href="signup.php">Register</a></li>       
+     </ul>
+  </div>
+  <div class="main">
+  <iframe id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="1100"
+    height="680"
+    src="https://www.emu.edu.tr/en">
+</iframe>
+     </div>
+   </div>
+</div>
+</body>
+</html>
